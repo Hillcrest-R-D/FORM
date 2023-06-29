ebd0546691ff3a810f19f60fe9492ee9261fd141
unknown <palsceo@gmail.com>
2023-06-28 21:52:24 -0400

    Sketching ideas for FORM 2.0

--- Lib/ORM.fs
+++ Lib/ORM.fs
@@ -142,6 +142,92 @@ module Orm =
                     |> Seq.toArray
                     |> makeEntity 
         } 
+    
+    // [Ok Articles; Ok User;]
+
+    // type Query = Context -> DbConnection -> DbTransaction -> ( string | DbCommand ) seq -> Result<IDataReader, exn> seq
+    // type Command = Context -> DbConnection -> DbTransaction -> ( string | DbCommand ) seq -> Result<int, exn> seq
+    // type Query = DbCommand -> DbConnection -> DbTransaction -> IDataReader | int
+    // module Unbatchable =
+    //     let selectAll<^T> db =
+    //         Orm.selectAll<^T> db |> executeAndConsume<^T> // -> Result<seq<User>, exn>
+    // insert<User> user db |> execute // -> Result<int, exn> 
+
+    // batch [insert<Article> article; insert<ArticleUser> articleUser; selectWhere<> "whatever"] db // -> Result 
+    (*
+        fun context -> 
+            fun connection ->
+                fun transaction ->
+                    fun querys ->
+                        let cmds = querys |> Seq.map toCmd
+                        let mutable errored = false
+                        seq { for cmd in cmds do 
+                            if not errored 
+                            then 
+                                match cmd.Execute() with 
+                                | Ok state -> 
+                                    yield Ok state 
+                                | Error e -> 
+                                    errored <- true
+                                    transaction.Rollback()
+                                    yield Error e
+                                    
+                                [Ok 1; Ok IDataReader; Error e]
+                        }
+                        type QueryReturn =
+                            | int
+                            | IDataReader
+                        
+                        |> Seq.takeWhile
+                            match query with 
+                            | str -> buildCommand(str)
+                            | cmd -> ()
+                            |> fun cmd -> cmd.Execute
+                            |> function 
+                            | Ok _ -> true
+                            | Error _ -> 
+                                transaction.Rollback()
+                                false
+                        |> Seq.fold (
+                            fun element state ->
+                                match state with 
+                                | Ok state ->
+                                    state @ element.Execute() |> Ok
+                                | Error e -> 
+                                    transaction.Rollback
+                                    Error e
+                        [Ok 1; Ok IDataReader; Error exn; Error exn]
+                        ) (Ok 0)
+    *)                       
+
+    // consumeReaders : seq<QueryResults> -> seq<>
+    //     match input with 
+    //     | IDataReader ->
+    //         consume it
+    //     | int ->
+    //        Error exn "Can't consume int"
+    //     [Ok 1]
+    // type QueryResult =
+    //     | Int of int 
+    //     | DataReader of IDataReader * Type
+    //     | Thing of obj * Type
+    // batch seq {insert<User> user db; selectWhere<User> "id = 1" db, User; delete<User> user db} // [Int 1; (DataReader dr, User); (DataReader dr, Article); Int 1]
+    // |> map 
+    //     function
+    //     | Ok el ->
+    //         match el with 
+    //         | Int i -> Int i
+    //         | DataReader (dr,type_) -> Thing <| consumeReader<type_> dr, type_  
+    
+    // match Unbatchable.selectAll<User> db with 
+    // | Ok result -> 
+    //     match result with 
+    //     | Int i -> 
+    //     | Reader reader -> consumeReader<User> reader
+    // | Error e -> Error e
+
+    // Batchable.selectAll<User> db
+    
 
     let inline getParamChar state = 
         match state with
@@ -149,15 +235,13 @@ module Orm =
         | MySQL _ -> "$"
         | PSQL _ -> "@"
         | SQLite _ -> "@"   
-
-    // let inline insertBase< ^T >
         
-    let inline insertBase< ^T > ( state : OrmState ) =
+    let inline insertBase< ^T > insertKeys ( state : OrmState ) =
         let paramChar = getParamChar state
         let tableName = ( table< ^T > state ) 
         let cols = 
             mapping< ^T > state 
-            |> Seq.filter (fun col -> not col.IsIndex && not col.IsKey )
+            |> Seq.filter (fun col -> insertKeys || not col.IsKey )
         let placeHolders = 
             cols
             |> Seq.mapi ( fun i x -> sprintf "%s%s" paramChar x.FSharpName )
@@ -172,12 +256,12 @@ module Orm =
     //Insert Into table1 Values
     // ( $1, $2, $3 ),
     // ( $4, $5, $6 ),
-    let inline insertManyBase< ^T > ( instances : ^T seq ) ( state : OrmState ) =
+    let inline insertManyBase< ^T > insertKeys ( instances : ^T seq ) ( state : OrmState ) =
         let paramChar   = getParamChar     state
         let tableName   = table< ^T >       state 
         let cols = 
             mapping< ^T > state 
-            |> Seq.filter (fun col -> not col.IsIndex && not col.IsKey )
+            |> Seq.filter (fun col -> insertKeys || not col.IsKey )
         let columns     = columns< ^T >     state
         let placeHolders = 
             instances 
@@ -338,11 +422,11 @@ module Orm =
         | SQLite _ -> SqliteParameter( )
     
     
-    let inline insert< ^T > ( instance : ^T )  ( state : OrmState ) =
+    let inline insert< ^T > insertKeys ( instance : ^T )  ( state : OrmState ) =
         match connect state with
         | Ok conn ->
             conn.Open( )
-            let query = insertBase< ^T > state
+            let query = insertBase< ^T > insertKeys state
             let paramChar = getParamChar state 
             use cmd = paramaterizeCmd query conn instance state//makeCommand query conn state
             
@@ -358,11 +442,11 @@ module Orm =
             result
         | Error e -> Error e
 
-    let inline insertMany< ^T > ( instances : ^T seq )  ( state : OrmState ) =
+    let inline insertMany< ^T > insertKeys ( instances : ^T seq )  ( state : OrmState ) =
         match connect state with
         | Ok conn -> 
             conn.Open( )
-            let query = insertManyBase< ^T > instances state
+            let query = insertManyBase< ^T > insertKeys instances state
             let paramChar = ( getParamChar state )
             let numCols = columns< ^T > state |> Seq.length
             use cmd = paramaterizeSeqCmd query conn instances state//makeCommand query conn state
