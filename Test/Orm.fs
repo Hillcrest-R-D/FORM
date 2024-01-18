676f9ba49b9e7662d0d2400c9a1ea1387c153a8d
unknown <palsceo@gmail.com>
2024-01-17 22:23:45 -0500

    Headaches with NUnit

--- Test/Orm.fs
+++ Test/Orm.fs
@@ -3,14 +3,167 @@ module Test.Orm
 open Form
 open Form.Attributes
 open Form.Utilities
-open HCRD.FORM.Tests.Setup 
 open NUnit.Framework
+
+System.IO.File.ReadAllLines("../../../../.env")
+|> Array.iter( fun line -> 
+    let chunks = line.Split("=")
+    let variable = chunks[0]
+    let value = System.String.Join("=", chunks[1..])
+    printfn "%A %A" variable value
+    System.Environment.SetEnvironmentVariable(variable, value)
+)
+
+let psqlConnectionString  = System.Environment.GetEnvironmentVariable("postgres_connection_string")
+let odbcConnectionString  = System.Environment.GetEnvironmentVariable("odbc_connection_string")
+let mysqlConnectionString  = ""
+let mssqlConnectionString  = ""
+let sqliteConnectionString  = System.Environment.GetEnvironmentVariable("sqlite_connection_string")
+
+printfn "Postgres: %A" (psqlConnectionString )
+printfn "ODBC: %A" (odbcConnectionString )
+printfn "SQLite: %A" (sqliteConnectionString )
+
+
+
+type Contexts =
+    | PSQL = 1
+    | MySQL = 2
+    | MSSQL = 4
+    | SQLite = 8
+    | ODBC = 16
+
+let psqlState  =     PSQL( psqlConnectionString , Contexts.PSQL )
+let mysqlState  =    MySQL( mysqlConnectionString , Contexts.MySQL )
+let mssqlState  =    MSSQL( mssqlConnectionString , Contexts.MSSQL )
+let sqliteState  =   SQLite( sqliteConnectionString , Contexts.SQLite )
+let odbcState  = ODBC(  odbcConnectionString , Contexts.ODBC )
+
+[<Table("SubFact", Contexts.PSQL)>]
+[<Table("SubFact", Contexts.MySQL)>]
+[<Table("SubFact", Contexts.MSSQL)>]
+[<Table("SubFact", Contexts.SQLite)>]
+[<Table("SubFact", Contexts.ODBC)>]
+type SubFact = 
+    {
+        factId : int64 
+        subFact : string
+    }
+
+[<Table("Fact", Contexts.PSQL)>]
+[<Table("Fact", Contexts.MySQL)>]
+[<Table("Fact", Contexts.MSSQL)>]
+[<Table("Fact", Contexts.SQLite)>]
+[<Table("Fact", Contexts.ODBC)>]
+type Fact =
+    {
+        [<Id(Contexts.PSQL)>]
+        [<Id(Contexts.SQLite)>]
+        [<Id(Contexts.MySQL)>]
+        [<Id(Contexts.MSSQL)>]
+        [<Id(Contexts.ODBC)>]
+        [<On(typeof<SubFact>, "factId", JoinDirection.Left, Contexts.PSQL)>]
+        [<On(typeof<SubFact>, "factId", JoinDirection.Left, Contexts.SQLite)>]
+        [<On(typeof<SubFact>, "factId", JoinDirection.Left, Contexts.ODBC)>]
+        indexId: int64
+        [<PrimaryKey("pk",Contexts.PSQL)>]
+        [<PrimaryKey("pk",Contexts.MySQL)>]
+        [<PrimaryKey("pk",Contexts.MSSQL)>]
+        [<PrimaryKey("pk",Contexts.SQLite)>]
+        [<PrimaryKey("pk",Contexts.ODBC)>]
+        id: string
+        [<Column("psqlName", Contexts.PSQL)>]
+        [<Column("mysqlName", Contexts.MySQL)>]
+        [<Column("mssqlName", Contexts.MSSQL)>]
+        [<Column("sqliteName", Contexts.SQLite)>]
+        [<Column("sqliteName", Contexts.ODBC)>]
+        [<SQLType("varchar(16)", Contexts.PSQL)>]
+        [<SQLType("varchar(16)", Contexts.MySQL)>]
+        [<SQLType("varchar(16)", Contexts.MSSQL)>]
+        [<SQLType("varchar(16)", Contexts.ODBC)>]
+        // [<SQLType("varchar(16)", Contexts.SQLite)>] !!! Won't work, sqlite doesn't have varchar
+        name: string 
+        [<Constraint("DEFAULT GETDATE()", Contexts.MSSQL)>]
+        [<Constraint("DEFAULT CURRENT_TIMESTAMP", Contexts.PSQL)>]
+        [<Constraint("DEFAULT CURRENT_TIMESTAMP()", Contexts.MySQL)>]
+        [<Constraint("DEFAULT CURRENT_TIMESTAMP", Contexts.SQLite)>]
+        [<Constraint("DEFAULT CURRENT_TIMESTAMP", Contexts.ODBC)>]
+        timeStamp: string
+        [<Unique("group1", Contexts.PSQL)>]
+        [<Unique("group1", Contexts.ODBC)>]
+        specialChar : string
+        [<SQLType("boolean", Contexts.PSQL)>]
+        [<SQLType("boolean", Contexts.ODBC)>]
+        maybeSomething : string 
+        [<Unique("group1", Contexts.PSQL)>]
+        [<Unique("group1", Contexts.ODBC)>]
+        sometimesNothing : int64 option
+        [<Unique("group2", Contexts.PSQL)>]
+        [<Unique("group2", Contexts.ODBC)>]
+        biteSize : string
+        [<ByJoin(typeof<SubFact>, Contexts.SQLite)>]
+        [<ByJoin(typeof<SubFact>, Contexts.PSQL)>]
+        [<ByJoin(typeof<SubFact>, Contexts.ODBC)>]
+        subFact : string option
+    }
+
+    //lookup = { id =  Orm.Node (  {_type = typeof<int>; value = 1 }, Orm.Leaf  { _type= typeof<string>; value = indexId }); value = None}
+    // member Relationship (lookup) = 
+    //     ^A 
+
+    // static member Relation (id, indexId) =
+    //     { id =  Orm.Node (  {_type = typeof<int>; value = id }, Orm.Leaf  { _type= typeof<string>; value = indexId }); value = None}
+
+module Fact = 
+    let init () = 
+        {
+            indexId = 1L
+            id = System.Guid.NewGuid().ToString()
+            name = "Gerry McGuire"
+            timeStamp = System.DateTime.Now.ToString()
+            specialChar = "Δ"
+            maybeSomething = "true"
+            sometimesNothing = Some 1L
+            biteSize =  "!aBite"
+            subFact = Some "sooper dooper secret fact"
+        }
+
+    
+type SerializedLogger() =
+
+    // create the mailbox processor
+    let agent = MailboxProcessor.Start(fun inbox ->
+
+        // the message processing function
+        let rec messageLoop () = async{
+
+            // read a message
+            let! msg = inbox.Receive()
+
+            // write it to the log
+            printfn "%A" msg
+
+            // loop to top
+            return! messageLoop ()
+            }
+
+        // start the loop
+        messageLoop ()
+        )
+
+    // public interface
+    member _.Log msg = agent.Post msg
+
+// test in isolation
+let logger = SerializedLogger()
+
+
 
 type FixtureArgs =
     static member Source : obj seq =
         seq {
             [| sqliteState |] 
-            // [| psqlState |]
+            [| psqlState |]
             // [| odbcState |]
         }
         
@@ -18,14 +171,13 @@ type FixtureArgs =
 type OrmSetup () = 
     [<OneTimeSetUp>]
     member _.Setup () = 
-        DotNetEnv.Env.Load() |> printf "Loaded variables %A" 
         printfn "sqlite - %A" (System.Environment.GetEnvironmentVariable("sqlite_connection_string"))
-        printfn "sqlite - %A" (System.Environment.GetEnvironmentVariable("postgres_connection_string"))
-        printfn "sqlite - %A" (System.Environment.GetEnvironmentVariable("odbc_connection_string"))
+        printfn "postgres - %A" (System.Environment.GetEnvironmentVariable("postgres_connection_string"))
+        printfn "odbc - %A" (System.Environment.GetEnvironmentVariable("odbc_connection_string"))
 
 [<TestFixtureSource(typeof<FixtureArgs>, "Source")>]
 type Orm (_testingState) =
-    let testingState = _testingState ()
+    let testingState = _testingState
     let tableName = "\"Fact\""
     let testGuid1 = System.Guid.NewGuid().ToString()
     let testGuid2 = System.Guid.NewGuid().ToString()
--- Test/Program.fs
+++ Test/Program.fs
@@ -3,40 +3,9 @@ open Form
 open Form.Orm
 open Form.Attributes
 open Form.Utilities
-open Setup
-module Main = 
-    type Contexts =
-        | Test1 = 0
 
-    let mssql = MSSQL("", Contexts.Test1)
-    [<Table("Temp.Test", Contexts.Test1)>]
-        type Test =
-            { Col1 : int 
-              Col2 : int 
-            }
+module Main = 
 
     [<EntryPoint>]
     let main _ = 
-        DotNetEnv.Env.Load "../" |> ignore
-        let testId = 1
-        let testIdSeq = seq{1}
-        // printfn "int to seq: %A\n int to seq to seq: %A" (Seq.concat [[testId]]) (Seq.red [[testIdSeq]]) 
-
-        let psqlConnectionString = 
-            System.Environment.GetEnvironmentVariable("postgres_connection_string")
-        let psqlState =     PSQL( psqlConnectionString, Contexts.PSQL )
-        if typedefof<int64> <> typedefof<seq<obj>> then printfn "match"
-
-        // let rel : Orm.Relation<int64, Fact>  = { id = 1; value = None}
-        // let test = (Orm.Relation<int64,Fact>.Value rel psqlState).value
-        columnMapping<Fact> <| sqliteState()
-        |> printfn "%A"
-
-        queryBase<Fact> <| sqliteState()
-        |> printfn "%A"
-
         0
-
-
-
-
