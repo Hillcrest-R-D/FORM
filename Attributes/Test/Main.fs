3c163181be757ceb00bc0c98c415ea810b6f6e83
unknown <palsceo@gmail.com>
2025-05-22 21:40:34 -0400

    Not sure :# Please enter the commit message for your changes. Lines starting

--- Attributes/Test/Main.fs
+++ Attributes/Test/Main.fs
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-﻿module Test
-open Expecto
-
-[<EntryPoint>]
-let main argv =
-    Tests.runTestsInAssembly defaultConfig argv
-=======
 ﻿namespace Form.Attributes.Test
 open Expecto
 
@@ -14,4 +6,3 @@ module Main =
     [<EntryPoint>]
     let main argv =
         Tests.runTestsInAssemblyWithCLIArgs [] argv
->>>>>>> ef3c08d919ff69869e7858e66b1944d429bf46fb
